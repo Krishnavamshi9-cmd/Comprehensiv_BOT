b5d215e35855faba29745a822945253f7f929dd7
Krishnavamshi9-cmd <krishnavamshikv9866@gmail.com>
2025-10-03 17:51:07 +0530

    second Commit

--- angular-frontend/src/main.ts
+++ angular-frontend/src/main.ts
@@ -1,10 +1,16 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { importProvidersFrom } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 bootstrapApplication(AppComponent, {
   providers: [
-    importProvidersFrom(HttpClientModule)
+    importProvidersFrom(
+      BrowserAnimationsModule,
+      HttpClientModule,
+      ReactiveFormsModule
+    )
   ]
-}).catch((err: any) => console.error(err));
+}).catch(err => console.error(err));
